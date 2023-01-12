6a6bcb9fc592d954dac7a7e80f079ff1f999d51f
anthony.lai <anthony.lai@varmeego.com>
2023-01-12 13:30:38 +0000

    update for the timeDiff checking

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,12 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 import './index.css';
 import { Divider, Row, Col } from "antd";
+import moment from 'moment';
 
 export enum RespType {
     Text,
     Json
 }
-
-type Right = {
-    summary: string;
-}
-
-const noRight: Right = {
-    summary: ''
-};
 
 type Time = {
     properties: {
@@ -32,73 +25,62 @@ const App: React.FC = () => {
     const [metrics, setMetrics] = useState(String);
     const [loading, setLoading] = useState(false);
 
+    function TimeDiff(time: Time | undefined){
+        if(time != undefined){
+            var startDate = moment.unix(time.properties.epoch.type/1000).format("hh:mm:ss");
+            var endDate = moment.unix(new Date().getTime()/1000).format("hh:mm:ss");
+
+            const start = moment(startDate, 'hh:mm:ss');
+            const end = moment(endDate, 'hh:mm:ss');
+
+            return moment.utc(moment(end,"HH:mm:ss").diff(moment(start,"HH:mm:ss"))).format("HH:mm:ss");
+        }
+
+    }
+
+    function Fetch(url: string, type: RespType) {
+        setLoading(true);
+
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'mysecrettoken',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then(async response => {
+                const isJson = response.headers.get('content-type')?.includes('application/json');
+                if (type == RespType.Json) {
+                    const result = isJson && await response.json();
+                    setTime(result);
+                } else if (type == RespType.Text) {
+                    const result = await response.text()
+                    setMetrics(result.replace(/\n/g, "<br />"));
+                }
+
+                // check for error response
+                if (!response.ok) {
+                    // get error message from body or default to response status
+                    return Promise.reject(response.status);
+                }
+                setLoading(false);
+            })
+            .catch(error => {
+                setLoading(false);
+                console.error('There was an error!', error);
+            })
+    }
+
     useEffect(() => {
 
         Fetch("http://localhost:4000/metrics", RespType.Text);
-        setInterval(() => Fetch("http://localhost:4000/metrics", RespType.Text), 10000)
-
-
         Fetch("http://localhost:4000/time", RespType.Json);
-        // setInterval(() => Fetch("http://localhost:4000/time", RespType.Json), 30000)
 
-        let check = time?.properties.epoch.type;
-        console.log("check: "+check);
+        setInterval(() => Fetch("http://localhost:4000/metrics", RespType.Text), 10000)
+        setInterval(() => Fetch("http://localhost:4000/time", RespType.Json), 10000)
 
-        // fetch("http://localhost:4000/time", {
-        //     method: 'GET',
-        //     headers: {
-        //         'Authorization': 'mysecrettoken',
-        //         'Access-Control-Allow-Origin': '*'
-        //     }
-        // })
-        //     .then(async response => {
-        //         const isJson = response.headers.get('content-type')?.includes('application/json');
-        //         const data = isJson && await response.json();
-
-        //         console.log("time: " + JSON.stringify(data));
-        //         // check for error response
-        //         if (!response.ok) {
-        //             // get error message from body or default to response status
-        //             const error = (data && data.message) || response.status;
-        //             return Promise.reject(error);
-        //         }
-
-        //     })
-        //     .catch(error => {
-        //         console.error('There was an error!', error);
-        //     });
-        function Fetch(url: string, type: RespType) {
-            setLoading(true);
-
-            fetch(url, {
-                method: 'GET',
-                headers: {
-                    'Authorization': 'mysecrettoken',
-                    'Access-Control-Allow-Origin': '*'
-                }
-            })
-                .then(async response => {
-                    const isJson = response.headers.get('content-type')?.includes('application/json');
-                    if (type == RespType.Json) {
-                        const result = isJson && await response.json();
-                        setTime(result);
-                    } else if (type == RespType.Text) {
-                        const result = await response.text()
-                        setMetrics(result.replace(/\n/g, "<br />"));
-                    }
-
-                    // check for error response
-                    if (!response.ok) {
-                        // get error message from body or default to response status
-                        return Promise.reject(response.status);
-                    }
-                    setLoading(false);
-                })
-                .catch(error => {
-                    setLoading(false);
-                    console.error('There was an error!', error);
-                })
-        }
+        
+        
     }, [])
 
     return (
@@ -107,7 +89,8 @@ const App: React.FC = () => {
                 (<div>Loading...</div>) : (
                     <Row>
                         <Col span={11}>Fetched value for server time <br />
-                        {time?.properties.epoch.description}  <br /></Col>
+                        {time?.properties.epoch.description}  <br />
+                        Time different between server and client: {TimeDiff(time)}  <br /></Col>
                         <Col className='height-500'>
                             <Divider type="vertical" className='height-100' />
                         </Col>
